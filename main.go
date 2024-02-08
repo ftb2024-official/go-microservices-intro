153bc5a4f430314114c0e9964e42f2f2146311d1
ftb2024_official <ftb2024@gmail.com>
2024-02-08 12:47:19 +0500

    simple go web server

--- main.go
+++ main.go
@@ -1,3 +1,28 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		log.Println("Hello, world!")
+
+		data, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Ooops", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(rw, "Hello, %s\n", data)
+	})
+
+	http.HandleFunc("/bye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Goodbye, world!")
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
